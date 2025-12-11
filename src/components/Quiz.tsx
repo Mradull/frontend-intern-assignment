771e8db8b8863a0d5bdaaec64be77ec592d8bdc7
Mradull <mradul.kapoor2004@gmail.com>
2025-12-12 02:09:43 +0530

    Fix: TS errors, updated UI

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import  { useMemo, useState } from "react";
 import QuestionCard from "./QuestionCard";
 import Progress from "./Progress";
 import Result from "./Result";
